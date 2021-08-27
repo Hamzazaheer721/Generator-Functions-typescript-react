ccd6e328353e5218ed7a1e2efe02b970f435a537
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-27 13:33:03 +0500

    !feat: color change feature has been added

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useState } from "react";
+import { FC, memo, useCallback, useEffect, useState } from "react";
 import initializeIterator from "./helper";
 import colorObjectType from "./types";
 import { MainContainer, Wrapper } from "./styled.index";
@@ -6,9 +6,19 @@ import { MainContainer, Wrapper } from "./styled.index";
 const HomeComponent: FC = memo(() => {
   const [colorIterator, setColorIterator] = useState<IterableIterator<colorObjectType>>(initializeIterator());
   const [colorState, changeColorState] = useState<colorObjectType>();
-  const handleColorChange = () => {
 
-  }
+  const resetIterator = useCallback(() => {
+    setColorIterator(initializeIterator)
+  }, [])
+
+  const handleColorChange = useCallback(() => {
+    changeColorState(colorIterator.next().value)
+  }, [])
+
+  useEffect(() => {
+    !colorState && resetIterator();
+  }, [colorState])
+
   return (
     <MainContainer colorObject={colorState}>
       <Wrapper type="button" onClick={handleColorChange}>
