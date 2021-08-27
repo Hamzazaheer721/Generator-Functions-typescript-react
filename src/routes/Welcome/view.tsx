19f1a6c54d60d86641f2471005511c134f5627df
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-27 19:28:33 +0500

    !fix: deleted unnecessary files

--- src/routes/Welcome/view.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import WelcomeComponent from '../../components/Welcome';
-
-const ViewComponent: FC<{}> = () => (
-  <WelcomeComponent />
-)
-
-export default ViewComponent;
